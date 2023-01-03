8f6bbff5b30af03fbb4c827d2d5d35598fbb2ea5
AntonioLoco <antoniolocorotondo23@gmail.com>
2023-01-03 13:38:02 +0100

    fixed keyboard

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import React, {useState, useRef} from 'react';
-import { StyleSheet, Text, TextInput, View, TouchableOpacity, ScrollView, Dimensions} from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, TextInput, View, TouchableOpacity, ScrollView, Dimensions, Keyboard} from 'react-native';
 import Todo from './components/Todo';
 
 export default function App() {
   const [todoList, setToDoList] = useState(["Fare colazione", "Andare a fare la spesa", "Studiare", "Studiare react"]);
   const [newTask, setNewTask] = useState("");
+  let heightHeader = 80;
+  let heightInput = 120;
+  const [positionInput, setPositionInput] = useState(0);
 
 
   const deleteTodo = (index) => {
@@ -25,24 +28,41 @@ export default function App() {
     }
   }
 
+  const openKeyboard = () => {
+    setPositionInput(310);
+  }
+
+  const closeKeyboard = () => {
+    setPositionInput(0);
+    Keyboard.dismiss();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>To do List</Text>
-      <ScrollView style={styles.wrapperTodo}>
-        {
-          todoList.map( (el, index) => <Todo text={el} key={index} delete={deleteTodo} index={index}/> )
-        }
-      </ScrollView>
-      <View style={styles.wrapperInput}> 
-        <TextInput 
-          placeholder='inserisci un task'
-          value={newTask}
-          onChangeText={ setNewTask }
-          style={styles.input}
-        />
-        <TouchableOpacity onPress={ () => addTodo()} style={styles.button}>
-          <Text style={styles.textButton}>+</Text>
-        </TouchableOpacity>
+    <View style={{width: "100%", height: Dimensions.get('window').height, backgroundColor: "#252525",position: "relative"}}>
+      <View style={[styles.container, {bottom: positionInput}]}>
+        <Text style={[styles.title, {marginTop: heightHeader}]}>To do List</Text>
+        <ScrollView style={[styles.wrapperTodo, {maxHeight: (Dimensions.get('window').height) - (heightHeader + heightInput)}]}>
+          {
+            todoList.map( (el, index) => <Todo text={el} key={index} delete={deleteTodo} index={index}/> )
+          }
+        </ScrollView>
+        <View style={[styles.wrapperInput, { height: heightInput}]}> 
+          <TextInput 
+            placeholder='inserisci un task'
+            value={newTask}
+            onChangeText={ setNewTask }
+            style={styles.input}
+            keyboardAppearance="dark"
+            onFocus={openKeyboard}
+            onEndEditing={closeKeyboard}
+          />
+          <TouchableOpacity onPress={ () => {
+            addTodo();
+            closeKeyboard()
+            }} style={styles.button}>
+            <Text style={styles.textButton}>+</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -50,36 +70,34 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#252525',
     position: "relative",
-    height: Dimensions.get('window').height
+    height: Dimensions.get('window').height,
+    position: "absolute",
+    left: 0,
+    right: 0
   },
   title: {
-    marginTop: 80,
     marginLeft: 30,
+    paddingBottom: 20,
     color: '#f5f5f5',
     fontSize: 25,
     fontWeight: 'bold'
   },
   wrapperTodo: {
     width: "90%",
-    maxHeight: (Dimensions.get('window').height) - 250, 
-    marginTop: 30,
     marginLeft: "auto",
-    marginRight: "auto",
+    marginRight: "auto"
   },
   wrapperInput: {
     position: "absolute",
     width: "100%",
-    height: 120,
     bottom: 0,
     left: 0,
     paddingHorizontal: 20,
-    paddingBottom: 50,
     flexDirection: "row",
     justifyContent: 'space-between',
-    alignItems: "flex-end",
+    alignItems: "center",
   },
   input: {
     width: "80%",
